f6a38a260a1a838349e4ff36dbb89330e0447138
Peng Guanwen <pg999w@outlook.com>
2019-10-27 00:37:11 +0800

    Add basic command line options

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
 #include "CLI/CLI.hpp"
+#include "config.h"
 
 int main(int argc, char **argv) {
-    CLI::App app("FIRASIM C++ Agent Client");
+    CLI::App app(PROJECT_DESCRIPTION);
+
+    auto print_version = [](int64_t count) {
+        std::cout << PROJECT_NAME << " " << PROJECT_VERSION << std::endl;
+        std::cout << PROJECT_DESCRIPTION << std::endl;
+        std::exit(0);
+    };
+    app.add_flag("-V, --version", print_version, "Prints version information");
 
     CLI11_PARSE(app, argc, argv);
 }
