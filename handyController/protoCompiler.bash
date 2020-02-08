ea91d23ca66629cda09e1982f8c9148fac999575
maralmd78 <maralmordad78@gmail.com>
2020-02-08 18:49:11 +0330

    [fixbug] fix automation protocompiler bug

--- handyController/protoCompiler.bash
+++ handyController/protoCompiler.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-rm -rf messages
-mkdir messages
-cd messages
-python3 -m grpc_tools.protoc -I=../../FIRAMessage --python_out=. ../../FIRAMessage/common.proto
-python3 -m grpc_tools.protoc -I=../../FIRAMessage --python_out=. ../../FIRAMessage/REF2CLI/messages.proto
-python3 -m grpc_tools.protoc -I=../../FIRAMessage --python_out=. --grpc_python_out=. ../../FIRAMessage/REF2CLI/service.proto
+rm common_pb2.py
+rm -rf REF2CLI
+python3 -m grpc_tools.protoc -I=../FIRAMessage --python_out=. ../FIRAMessage/common.proto
+python3 -m grpc_tools.protoc -I=../FIRAMessage --python_out=. ../FIRAMessage/REF2CLI/messages.proto
+python3 -m grpc_tools.protoc -I=../FIRAMessage --python_out=. --grpc_python_out=. ../FIRAMessage/REF2CLI/service.proto
